8bf8a26a16d9f499af17f51c06bea8871c9193d3
wilyamz <99989999+willyboy2001@users.noreply.github.com>
2022-11-13 22:25:08 +0300

    Updated Code. Added hash tables for easy access of symbols

--- VirtualMachineTranslator.py
+++ VirtualMachineTranslator.py
@@ -1,7 +1,7 @@
 class Parser:
     
     arithmetic_commands = {'add','sub','neg','eq','gt','lt','and','or','not'}
-    special_commands={'C_PUSH','C_POP','C_FUNCTION','C_CALL'}
+    special_commands={'C_PUSH','C_POP','C_FUNCTION','C_CALL','C_ARITHMETIC'}
     def __init__(self,filename): ## opens file for processing
         
         self.filename=open(filename,mode='r')
@@ -14,14 +14,10 @@ class Parser:
         self.filename.seek(original)
         
         if(curr==''):
-            self.end_of_file=True
             return False
         elif(curr[-1]=='\n' or (curr.strip() !='')):
              return True
-        self.end_of_file=True
         return False
-    
-    
     
     def advance(self):
         return self.filename.readline()
@@ -74,57 +70,177 @@ class Parser:
         
         
             
-        
-        
-        
-        
-        
-
 class CodeWriter:
+    memory_pointers={
+        'stack':0,
+        'local':1,
+        'argument':2,
+        'this':3,
+        'that':4,
+        'temp':5
+    }
+    operation_to_sign={
+        'neg':'-',
+        'not':'!',
+        'add':'+',
+        'sub':'-',
+        'and':'&',
+        'or':'|'
+    }
+    
+    operations_comparisons={
+        'eq':'JEQ',
+        'lt':'JLT',
+        'gt':'JGT'
+    }
+    
+    two_operands={'add','sub','and','or','gt','lt','eq'} #Operators with 2 operands
+    one_operand={'neg','not'} #Operator with one operand
     ##Generates assemebly code 
     def __init__(self,filename):
         self.filename= open(filename,mode='w')
+        self.number=0
+    
+    def add_comment(self,arr):
+        string_comment=f'   //{" ".join(arr)}\n'
+        self.filename.write(string_comment)
+        
+        
         
     def write_arithmetic(self,command):
-        string_comment=f'   //{command.upper()}\n'
+        self.add_comment(command)
+        lower=command[0].lower()
+        
+
+
         string_1='''@R0
 A=M-1
 '''
         string_op=string_3=''
-
-        if(command.lower()=='not'):
-            string_2='M=!M'
-        elif(command.lower()=='neg'):
-            string_2='M=-M'
+        
+        if(lower in CodeWriter.one_operand):
+            string_2=f'M={CodeWriter.operation_to_sign[lower]}M'
         else:
             string_op='''A=A-1
 D=M
 A=A+1
-'''            
-            if(command.lower()=='add'):
-                string_2='D=D+M'
-            elif(command.lower()=='sub'):
-                string_2='D=D-M'
-            elif(command.lower()=='and'): 
-                string_2='D=D&M'
-            elif(command.lower()=='or'):
-                string_2='D=D|M'
-            elif(command.lower()=='sub'):
-                string_2='D=D-M'     
-            else:
-                pass
-        
-            string_3= '''
+'''         
+            if(lower in CodeWriter.operation_to_sign and lower in CodeWriter.two_operands):
+            
+                string_2=f'''D=D{CodeWriter.operation_to_sign[lower]}M
 A=A-1
 M=D
-@R0
+'''  
+            else:
+                string_2= f'''D=D-M
+A=A-1
+M=D
+D=A
+@13
+M=D
+A=M
+D=M
+
+@write_1_{self.number}
+D;{CodeWriter.operations_comparisons[lower]}
+
+@13
+A=M
+M=0
+
+@end_of_condition_{self.number}
+0;JMP
+
+(write_1_{self.number})
+@13
+A=M
+M=-1
+
+(end_of_condition_{self.number})
+'''
+                self.number+=1
+                
+            string_3= '''@R0
 M=M-1
 '''
         
-        self.filename.write(string_comment+string_1+string_op+string_2+string_3+'\n');
+        self.filename.write(string_1+string_op+string_2+string_3+'\n');
             
     
     
+    def write_push_pop(self, arr):
+        self.add_comment(arr)
+        lower=arr[0].lower()
+        
+        if(arr[0].lower()=='pop'):
+            self.write_pop(arr)
+        else:
+            self.write_push(arr)        
+        
+        
+ 
+    def write_pop(self,arr):
+        lower= arr[1].lower()
+        string_non_static=''
+        if(lower in CodeWriter.memory_pointers):
+            string_non_static=f'''@{arr[2]}
+D=A
+@ {CodeWriter.memory_pointers[lower]}
+D=D+{'A' if lower=='temp' else 'M'}
+'''
+        string_static=f'''@{self.filename.name.title()[:-3]+f'{arr[2]}'}
+D=A
+'''
+        
+        string_compulsory=f'''@R13
+M=D
+@ {CodeWriter.memory_pointers['stack']}
+M=M-1
+A=M
+D=M
+@R13
+A=M
+M=D
+'''
+        if(arr[1].lower()=='static'):
+            self.filename.write(string_static)
+        else:
+            self.filename.write(string_non_static)
+            
+            
+        self.filename.write(string_compulsory)
+        
+            
+    
+    def write_push(self,arr):
+        lower= arr[1].lower()
+        str_otherwise=''
+        str_non_static=f'''@{arr[2]}
+D=A
+'''
+        str_static= f'''@{self.filename.name.title()[:-3]+f'{arr[2]}' }
+D=M
+'''
+        
+        if(lower in CodeWriter.memory_pointers):
+
+            str_otherwise=f'''@ {CodeWriter.memory_pointers[lower]}
+A=D+{'A' if lower=='temp' else 'M'}
+D=M
+'''
+        str_compulsory=f'''@{CodeWriter.memory_pointers['stack']}
+M=M+1
+A=M-1
+M=D
+'''
+        if(arr[1].lower()=='static'):
+            self.filename.write(str_static)
+        else:
+            self.filename.write(str_non_static)
+            if(arr[1].lower()!='constant'):
+                self.filename.write(str_otherwise)
+        self.filename.write(str_compulsory)
+        
     
     
     def close(self):
@@ -138,14 +254,14 @@ M=M-1
         
         self.filename.write(string)
         self.filename.close()
-
         
         
         
         
+        #VM Translatior
 def VM_Translator(file_name):
     parser = Parser(file_name)
-    codewriter= CodeWriter(file_name+'.asm') 
+    codewriter= CodeWriter(f'C:\\Users\\Admin\\Downloads\\nand2tetris\\nand2tetris\\projects\\07\\StackArithmetic\\StackTest\\{file_name[:-2]}asm') 
 
     while (parser.has_more_commands()):
         lis=parser.tokenize()
@@ -153,7 +269,14 @@ def VM_Translator(file_name):
             continue
         command = parser.get_command()
         if(command=='C_ARITHMETIC'):
-            codewriter.write_arithmetic(lis[0])
+            codewriter.write_arithmetic(lis)
+        else:
+            codewriter.write_push_pop(lis)
 
-    codewriter.close()      
-      
+    codewriter.close()
+    
+    
+    #Start of Execution
+    VM_Translator('BasicTest.vm')    
+
+    
